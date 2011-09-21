26a3c2e12b84e8e4bdf9ea04120b9014c48d1e15
Dietmar Maurer <dietmar@proxmox.com>
2011-09-21 12:26:59 +0200

    start after clvm, stop before clvm

--- qemu.init.d
+++ qemu.init.d
@@ -2,8 +2,8 @@
 
 ### BEGIN INIT INFO
 # Provides:          qemu-server
-# Required-Start:    pve-cluster cman $network $local_fs $remote_fs
-# Required-Stop:     pve-cluster cman $network $local_fs $remote_fs
+# Required-Start:    pve-cluster cman clvm $network $local_fs $remote_fs
+# Required-Stop:     pve-cluster cman clvm $network $local_fs $remote_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: start all qemu/kvm virtual machines
