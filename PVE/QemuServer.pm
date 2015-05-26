fd1f36ac6f7b92e5d2dc00d14a7b8c86cf91da00
Wolfgang Bumiller <w.bumiller@proxmox.com>
2015-05-26 16:08:53 +0200

    localhost instead of 127.0.0.1 makes ipv6 life easier

--- PVE/QemuServer.pm
+++ PVE/QemuServer.pm
@@ -3024,7 +3024,7 @@ sub config_to_command {
 	my $pfamily = PVE::Tools::get_host_address_family($nodename);
 	$spice_port = PVE::Tools::next_spice_port($pfamily);
 
-	push @$devices, '-spice', "tls-port=${spice_port},addr=127.0.0.1,tls-ciphers=DES-CBC3-SHA,seamless-migration=on";
+	push @$devices, '-spice', "tls-port=${spice_port},addr=localhost,tls-ciphers=DES-CBC3-SHA,seamless-migration=on";
 
 	push @$devices, '-device', "virtio-serial,id=spice$pciaddr";
 	push @$devices, '-chardev', "spicevmc,id=vdagent,name=vdagent";
