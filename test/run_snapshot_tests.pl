1c4afc63cda8b012e55aef8faf48235c68e08ded
Fabian Grünbichler <f.gruenbichler@proxmox.com>
2017-05-17 12:40:55 +0200

    tests: exit with -1 in case of failures
    Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

--- test/run_snapshot_tests.pl
+++ test/run_snapshot_tests.pl
@@ -6,5 +6,6 @@ use warnings;
 use TAP::Harness;
 
 my $harness = TAP::Harness->new( { "verbosity" => -2 });
-$harness->runtests( "snapshot-test.pm");
+my $res = $harness->runtests( "snapshot-test.pm");
 system( "rm -rf snapshot-working/");
+exit -1 if $res->{failed};
