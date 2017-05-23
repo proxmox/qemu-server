df58868200447066dfc8fc3de87238407699c3fc
Fabian Grünbichler <f.gruenbichler@proxmox.com>
2017-05-23 14:23:52 +0200

    print_cpu_device: remove unused variable

--- PVE/QemuServer.pm
+++ PVE/QemuServer.pm
@@ -1720,8 +1720,6 @@ sub print_cpu_device {
 	$cpu = $cpuconf->{cputype};
     }
 
-    my $sockets = 1;
-    $sockets = $conf->{sockets} if  $conf->{sockets};
     my $cores = $conf->{cores} || 1;
 
     my $current_core = ($id - 1) % $cores;
