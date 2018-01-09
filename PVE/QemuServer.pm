proxmox/qemu-server

commit 2bfbee039bbe92f6ac9e1c81b3f1be588ee5324c

include format for efidisk
if the efidisk is in 'raw' format, qemu will prevent writes
on block zero if the format is not explicitely given

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

PVE/QemuServer.pm
@@ -2901,6 +2901,7 @@ sub config_to_command {
 	die "uefi base image not found\n" if ! -f $OVMF_CODE;
 
 	my $path;
+	my $format = 'raw';
 	if (my $efidisk = $conf->{efidisk0}) {
 	    my $d = PVE::JSONSchema::parse_property_string($efidisk_fmt, $efidisk);
 	    my ($storeid, $volname) = PVE::Storage::parse_volume_id($d->{file}, 1);
@@ -2909,6 +2910,7 @@ sub config_to_command {
 	    } else {
 		$path = $d->{file};
 	    }
+	    $format = $d->{format} if $d->{format};
 	} else {
 	    warn "no efidisk configured! Using temporary efivars disk.\n";
 	    $path = "/tmp/$vmid-ovmf.fd";
@@ -2916,7 +2918,7 @@ sub config_to_command {
 	}
 
 	push @$cmd, '-drive', "if=pflash,unit=0,format=raw,readonly,file=$OVMF_CODE";
-	push @$cmd, '-drive', "if=pflash,unit=1,id=drive-efidisk0,file=$path";
+	push @$cmd, '-drive', "if=pflash,unit=1,format=$format,id=drive-efidisk0,file=$path";
     }
 
 

commit 13bca7b4deabfa11c4299a26c6f583af95d6bc4e

fix efi disk format detection
Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>
Fixes: 2bfbee039bbe ("include format for efidisk")
Reviewed-by: Thomas Lamprecht <t.lamprecht@proxmox.com>
Tested-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/QemuServer.pm
@@ -2908,20 +2908,27 @@ sub config_to_command {
 	die "uefi base image not found\n" if ! -f $OVMF_CODE;
 
 	my $path;
-	my $format = 'raw';
+	my $format;
 	if (my $efidisk = $conf->{efidisk0}) {
 	    my $d = PVE::JSONSchema::parse_property_string($efidisk_fmt, $efidisk);
 	    my ($storeid, $volname) = PVE::Storage::parse_volume_id($d->{file}, 1);
+	    $format = $d->{format};
 	    if ($storeid) {
 		$path = PVE::Storage::path($storecfg, $d->{file});
+		if (!defined($format)) {
+		    my $scfg = PVE::Storage::storage_config($storecfg, $storeid);
+		    $format = qemu_img_format($scfg, $volname);
+		}
 	    } else {
 		$path = $d->{file};
+		die "efidisk format must be specified\n"
+		    if !defined($format);
 	    }
-	    $format = $d->{format} if $d->{format};
 	} else {
 	    warn "no efidisk configured! Using temporary efivars disk.\n";
 	    $path = "/tmp/$vmid-ovmf.fd";
 	    PVE::Tools::file_copy($OVMF_VARS, $path, -s $OVMF_VARS);
+	    $format = 'raw';
 	}
 
 	push @$cmd, '-drive', "if=pflash,unit=0,format=raw,readonly,file=$OVMF_CODE";