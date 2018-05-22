f889aa0fbb79c71073f2a573184be142104ad5fb
Dominik Csapak <d.csapak@proxmox.com>
2018-05-22 16:44:33 +0200

    fix #1780: change datacenter.conf to datacenter.cfg
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- PVE/QemuServer.pm
+++ PVE/QemuServer.pm
@@ -272,7 +272,7 @@ my $confdesc = {
     keyboard => {
 	optional => 1,
 	type => 'string',
-	description => "Keybord layout for vnc server. Default is read from the '/etc/pve/datacenter.conf' configuration file.".
+	description => "Keybord layout for vnc server. Default is read from the '/etc/pve/datacenter.cfg' configuration file.".
 		       "It should not be necessary to set it.",
 	enum => PVE::Tools::kvmkeymaplist(),
 	default => undef,
