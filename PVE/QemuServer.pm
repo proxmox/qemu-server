15c6e277a4cdbaa9fedce5421d6dc4fab47cffd3
Oguz Bektas <o.bektas@proxmox.com>
2019-10-24 13:53:09 +0200

    fix #2367: do not allow snapshot with name PENDING
    or any other variant of the word 'pending'.
    
    note that we can actually allow this snapshot after PVE 7.0, since
    pending section and snapshots will be properly namespaced.
    ([pve:pending] and [snap:$snapname] or similar).
    
    Signed-off-by: Oguz Bektas <o.bektas@proxmox.com>

--- PVE/QemuServer.pm
+++ PVE/QemuServer.pm
@@ -2740,7 +2740,10 @@ sub write_vm_config {
     &$cleanup_config($conf->{pending}, 1);
 
     foreach my $snapname (keys %{$conf->{snapshots}}) {
-	die "internal error" if $snapname eq 'pending';
+	# TODO: we can allow snapshots with name 'pending' after PVE 7.0
+	# since pending section is namespaced with 'pve:'
+	# but for now, we should forbid it to avoid confusion in parser
+	die "internal error: snapshot name '$snapname' is forbidden" if lc($snapname) eq 'pending';
 	&$cleanup_config($conf->{snapshots}->{$snapname}, undef, $snapname);
     }
 
