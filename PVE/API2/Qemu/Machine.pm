36b02697244df8178f3f7c67106b2de4c968c352
Thomas Lamprecht <t.lamprecht@proxmox.com>
2021-03-05 16:33:08 +0100

    api: machine list: parse as JSON
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- PVE/API2/Qemu/Machine.pm
+++ PVE/API2/Qemu/Machine.pm
@@ -3,8 +3,10 @@ package PVE::API2::Qemu::Machine;
 use strict;
 use warnings;
 
-use PVE::RESTHandler;
+use JSON;
+
 use PVE::JSONSchema qw(get_standard_option);
+use PVE::RESTHandler;
 use PVE::Tools qw(file_get_contents);
 
 use base qw(PVE::RESTHandler);
@@ -28,22 +30,32 @@ __PACKAGE__->register_method({
 	type => 'array',
 	items => {
 	    type => 'object',
+	    additionalProperties => 1,
 	    properties => {
-		name => {
+		id => {
 		    type => 'string',
-		    description => "Name of machine type.",
+		    description => "Full name of machine type and version.",
+		},
+		type => {
+		    type => 'string',
+		    enum => ['q35', 'i440fx'],
+		    description => "The machine type.",
+		},
+		version => {
+		    type => 'string',
+		    description => "The machine version.",
 		},
 	    },
 	},
     },
     code => sub {
-	my $content = eval {
-	    file_get_contents("/usr/share/kvm/machine-versions-x86_64");
+	my $machines = eval {
+	    my $raw = file_get_contents('/usr/share/kvm/machine-versions-x86_64.json');
+	    return from_json($raw, { utf8 => 1 });
 	};
-	die "could not get supported machine versions (try updating 'pve-qemu-kvm') - $@" if $@;
-	my @data = split(m/\n/, $content);
-	@data = map { { name => $_ } } @data;
-	return \@data;
-    }});
+	die "could not load supported machine versions - $@\n" if $@;
+	return $machines;
+    }
+});
 
 1;
