proxmox/qemu-server

commit f9be9137fc87bd69e7f703a6c39d3475486db324

api: create/modify: add content type checks
Signed-off-by: Fabian Ebner <f.ebner@proxmox.com>

PVE/API2/Qemu.pm
@@ -104,7 +104,14 @@ my $check_storage_access = sub {
 	    raise_param_exc({ storage => "storage '$storeid' does not support vm images"})
 		if !$scfg->{content}->{images};
 	} else {
-	    PVE::Storage::check_volume_access($rpcenv, $authuser, $storecfg, $vmid, $volid);
+	    PVE::Storage::check_volume_access(
+		$rpcenv,
+		$authuser,
+		$storecfg,
+		$vmid,
+		$volid,
+		'images',
+	    );
 	}
     });
 
@@ -230,7 +237,14 @@ my $create_disks = sub {
 	    delete $disk->{format}; # no longer needed
 	    $res->{$ds} = PVE::QemuServer::print_drive($disk);
 	} else {
-	    PVE::Storage::check_volume_access($rpcenv, $authuser, $storecfg, $vmid, $volid);
+	    PVE::Storage::check_volume_access(
+		$rpcenv,
+		$authuser,
+		$storecfg,
+		$vmid,
+		$volid,
+		'images',
+	    );
 
 	    PVE::Storage::activate_volumes($storecfg, [ $volid ]) if $storeid;
 
@@ -645,7 +659,14 @@ __PACKAGE__->register_method({
 		die "pipe requires cli environment\n" if $rpcenv->{type} ne 'cli';
 		$archive = { type => 'pipe' };
 	    } else {
-		PVE::Storage::check_volume_access($rpcenv, $authuser, $storecfg, $vmid, $archive);
+		PVE::Storage::check_volume_access(
+		    $rpcenv,
+		    $authuser,
+		    $storecfg,
+		    $vmid,
+		    $archive,
+		    'backup',
+		);
 
 		$archive = $parse_restore_archive->($storecfg, $archive);
 	    }

commit db81c007548d2deec303f11a0e00f6986db95294

api: create/modify: fix content type checks by re-allowing iso images
Fixes: f9be9137 ("api: create/modify: add content type checks"
Reported-by: Thomas Lamprecht <t.lamprecht@proxmox.com>
Signed-off-by: Fabian Ebner <f.ebner@proxmox.com>

PVE/API2/Qemu.pm
@@ -142,14 +142,12 @@ my $check_storage_access = sub {
 	    raise_param_exc({ storage => "storage '$storeid' does not support vm images"})
 		if !$scfg->{content}->{images};
 	} else {
-	    PVE::Storage::check_volume_access(
-		$rpcenv,
-		$authuser,
-		$storecfg,
-		$vmid,
-		$volid,
-		'images',
-	    );
+	    PVE::Storage::check_volume_access($rpcenv, $authuser, $storecfg, $vmid, $volid);
+	    if ($storeid) {
+		my ($vtype) = PVE::Storage::parse_volname($storecfg, $volid);
+		raise_param_exc({ $ds => "content type needs to be 'images' or 'iso'" })
+		    if $vtype ne 'images' && $vtype ne 'iso';
+	    }
 	}
 
 	if (my $src_image = $drive->{'import-from'}) {
@@ -421,14 +419,12 @@ my $create_disks = sub {
 
 	    print "$ds: successfully created disk '$res->{$ds}'\n";
 	} else {
-	    PVE::Storage::check_volume_access(
-		$rpcenv,
-		$authuser,
-		$storecfg,
-		$vmid,
-		$volid,
-		'images',
-	    );
+	    PVE::Storage::check_volume_access($rpcenv, $authuser, $storecfg, $vmid, $volid);
+	    if ($storeid) {
+		my ($vtype) = PVE::Storage::parse_volname($storecfg, $volid);
+		die "cannot use volume $volid - content type needs to be 'images' or 'iso'"
+		    if $vtype ne 'images' && $vtype ne 'iso';
+	    }
 
 	    PVE::Storage::activate_volumes($storecfg, [ $volid ]) if $storeid;
 