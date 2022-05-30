e88ceecac1ccb6d99eb73dee7a12c7f1d40573de
Fabian Grünbichler <f.gruenbichler@proxmox.com>
2022-05-30 10:00:16 +0200

    fix uninitialized value
    if the configured display hardware has the (optional) default type, but
    some other attribute is set, this would match against `undef` and spew
    lots of warnings in the logs.
    
    Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

--- PVE/API2/Qemu.pm
+++ PVE/API2/Qemu.pm
@@ -2443,8 +2443,9 @@ __PACKAGE__->register_method({
 
 	if ($conf->{vga}) {
 	    my $vga = PVE::QemuServer::parse_vga($conf->{vga});
-	    $status->{spice} = 1
-		if $vga->{type} =~ /^virtio/ || PVE::QemuServer::vga_conf_has_spice($conf->{vga});
+	    my $spice = defined($vga->{type}) && $vga->{type} =~ /^virtio/;
+	    $spice ||= PVE::QemuServer::vga_conf_has_spice($conf->{vga});
+	    $status->{spice} = 1 if $spice;
 	}
 	$status->{agent} = 1 if PVE::QemuServer::get_qga_key($conf, 'enabled');
 
