proxmox/qemu-server

commit efa3aa24963a8e3fd2639fa6129393da0686121f

avoid list context for volume_size_info calls
With the recent pve-storage commit d70d814 ("api: fix get content call response
type for RBD/ZFS/iSCSI volumes"), the volume_size_info call for RBD in
list context is much slower than before (from a quick test, about twice as long
without snapshots, even longer with snapshots and untested, but when using an
external cluster with image not having the fast-diff feature, it should be worse
still) and thus increases the likelihood to run into timeouts here.

None of the callers here actually need the more expensive call, so just
avoid calling in list context.

Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

PVE/VZDump/QemuServer.pm
@@ -119,8 +119,11 @@ sub prepare {
 	}
 	next if !$path;
 
-	my ($size, $format) = eval { PVE::Storage::volume_size_info($self->{storecfg}, $volid, 5) };
+	my $size = eval { PVE::Storage::volume_size_info($self->{storecfg}, $volid, 5) };
 	die "no such volume '$volid'\n" if $@;
+
+	my $scfg = PVE::Storage::storage_config($self->{storecfg}, $storeid);
+	my $format = PVE::QemuServer::qemu_img_format($scfg, $volname);
 
 	my $diskinfo = {
 	    path => $path,

PVE/QemuServer.pm
@@ -8077,7 +8077,7 @@ sub clone_disk {
 	} else {
 	    clone_disk_check_io_uring($drive, $storecfg, $src_storeid, $storeid, $use_drive_mirror);
 
-	    ($size) = PVE::Storage::volume_size_info($storecfg, $drive->{file}, 10);
+	    $size = PVE::Storage::volume_size_info($storecfg, $drive->{file}, 10);
 	}
 	$newvolid = PVE::Storage::vdisk_alloc(
 	    $storecfg, $storeid, $newvmid, $dst_format, $name, ($size/1024)
@@ -8129,7 +8129,7 @@ sub clone_disk {
     }
 
 no_data_clone:
-    my ($size) = eval { PVE::Storage::volume_size_info($storecfg, $newvolid, 10) };
+    my $size = eval { PVE::Storage::volume_size_info($storecfg, $newvolid, 10) };
 
     my $disk = dclone($drive);
     delete $disk->{format};
@@ -8208,7 +8208,7 @@ sub create_efidisk($$$$$$$) {
     PVE::Storage::activate_volumes($storecfg, [$volid]);
 
     qemu_img_convert($ovmf_vars, $volid, $vars_size_b, undef, 0);
-    my ($size) = PVE::Storage::volume_size_info($storecfg, $volid, 3);
+    my $size = PVE::Storage::volume_size_info($storecfg, $volid, 3);
 
     return ($volid, $size/1024);
 }

commit 670f947ee86a9046fe2a5cdc74c7aa18e99a4cba

backup: prepare: fix format detection for disks without storage ID
which is the case for passed-through disks. The qemu_img_format()
function cannot correctly handle those, and it's not safe to assume
they are raw (it's most likely, but not guaranteed), so just use the
storage method for the format like it was done before commit
efa3aa24 ("avoid list context for volume_size_info calls"). This will
use 'qemu-img info' to get the actual format.

Reported in the community forum:
https://forum.proxmox.com/threads/124794/
https://forum.proxmox.com/threads/124823/
https://forum.proxmox.com/threads/124818/

Fixes: efa3aa24 ("avoid list context for volume_size_info calls")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Tested-by: Friedrich Weber <f.weber@proxmox.com>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/VZDump/QemuServer.pm
@@ -119,11 +119,20 @@ sub prepare {
 	}
 	next if !$path;
 
-	my $size = eval { PVE::Storage::volume_size_info($self->{storecfg}, $volid, 5) };
-	die "no such volume '$volid'\n" if $@;
+	my ($size, $format);
+	if ($storeid) {
+	    # The call in list context can be expensive for certain plugins like RBD, just get size
+	    $size = eval { PVE::Storage::volume_size_info($self->{storecfg}, $volid, 5) };
+	    die "no such volume '$volid'\n" if $@;
 
-	my $scfg = PVE::Storage::storage_config($self->{storecfg}, $storeid);
-	my $format = PVE::QemuServer::qemu_img_format($scfg, $volname);
+	    my $scfg = PVE::Storage::storage_config($self->{storecfg}, $storeid);
+	    $format = PVE::QemuServer::qemu_img_format($scfg, $volname);
+	} else {
+	    ($size, $format) = eval {
+		PVE::Storage::volume_size_info($self->{storecfg}, $volid, 5);
+	    };
+	    die "no such volume '$volid'\n" if $@;
+	}
 
 	my $diskinfo = {
 	    path => $path,