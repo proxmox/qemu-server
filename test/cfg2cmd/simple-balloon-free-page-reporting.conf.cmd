ac7c615b5059ee1f32e7476585a8877aae02ea45
Fiona Ebner <f.ebner@proxmox.com>
2024-07-23 17:25:47 +0200

    test: cfg2cmd: don't use QEMU binary version less than 7.0 for tests
    Since there are certain checks that depend on the QEMU binary version,
    tests with a fixed QEMU binary version make it less likely to catch
    issues on current setups, because for those, the QEMU binary version
    will always be higher than in the tests.
    
    Two of the affected tests explicitly mention the version, so set the
    machine version for those. For the other two, there's no real
    requirement to test for a specific machine version either, so just use
    the latest.
    
    Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
    Reviewed-by: Daniel Kral <d.kral@proxmox.com>
    Tested-by: Daniel Kral <d.kral@proxmox.com>

--- test/cfg2cmd/simple-balloon-free-page-reporting.conf.cmd
+++ test/cfg2cmd/simple-balloon-free-page-reporting.conf.cmd
@@ -30,4 +30,4 @@
   -device 'scsi-hd,bus=scsihw0.0,channel=0,scsi-id=0,lun=0,drive=drive-scsi0,id=scsi0,bootindex=100' \
   -netdev 'type=tap,id=net0,ifname=tap8006i0,script=/var/lib/qemu-server/pve-bridge,downscript=/var/lib/qemu-server/pve-bridgedown,vhost=on' \
   -device 'virtio-net-pci,mac=A2:C0:43:77:08:A0,netdev=net0,bus=pci.0,addr=0x12,id=net0,bootindex=300' \
-  -machine 'type=pc+pve0'
+  -machine 'type=pc-i440fx-6.2+pve0'
