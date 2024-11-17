bd62724fcd16ec12f53e8c0c3aba5b863447bb55
Thomas Lamprecht <t.lamprecht@proxmox.com>
2024-11-17 18:43:15 +0100

    query-machine-capabilities: fix indentation
    do not propagate that absolute mess of mixing tabs and spaces to new
    programs that ain't perl and thus doesn't need to suffer.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- query-machine-capabilities/query-machine-capabilities.c
+++ query-machine-capabilities/query-machine-capabilities.c
@@ -12,8 +12,8 @@ int main() {
     // https://en.wikipedia.org/wiki/CPUID#EAX=8000001Fh:_Encrypted_Memory_Capabilities
     uint32_t query_function = 0x8000001F;
     asm volatile("cpuid"
-	 : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
-	 : "0"(query_function)
+         : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+         : "0"(query_function)
     );
 
     bool sev_support = (eax & (1<<1)) != 0;
@@ -28,51 +28,51 @@ int main() {
     struct stat statbuf;
     int ret = stat(path, &statbuf);
     if (ret == 0) {
-	if (!S_ISDIR(statbuf.st_mode)) {
-	    printf("Path %s is not a directory.\n", path);
-	    return 1;
-	}
+        if (!S_ISDIR(statbuf.st_mode)) {
+            printf("Path %s is not a directory.\n", path);
+            return 1;
+        }
     } else if (errno == ENOENT) {
-	if (mkdir(path, 0755) != 0) {
-	    printf("Error creating directory %s: %s\n", path, strerror(errno));
-	    return 1;
-	}
+        if (mkdir(path, 0755) != 0) {
+            printf("Error creating directory %s: %s\n", path, strerror(errno));
+            return 1;
+        }
     } else {
-	printf("Error checking path %s: %s\n", path, strerror(errno));
-	return 1;
+        printf("Error checking path %s: %s\n", path, strerror(errno));
+        return 1;
     }
 
     FILE *file;
     const char *filename = "/run/qemu-server/host-hw-capabilities.json";
     file = fopen(filename, "w");
     if (file == NULL) {
-	perror("Error opening file");
-	return 1;
+        perror("Error opening file");
+        return 1;
     }
 
     ret = fprintf(file,
-	"{"
-	" \"amd-sev\": {"
-	" \"cbitpos\": %u,"
-	" \"reduced-phys-bits\": %u,"
-	" \"sev-support\": %s,"
-	" \"sev-support-es\": %s,"
-	" \"sev-support-snp\": %s"
-	" }"
-	" }\n",
-	cbitpos,
-	reduced_phys_bits,
-	sev_support ? "true" : "false",
-	sev_es_support ? "true" : "false",
-	sev_snp_support ? "true" : "false"
+        "{"
+        " \"amd-sev\": {"
+        " \"cbitpos\": %u,"
+        " \"reduced-phys-bits\": %u,"
+        " \"sev-support\": %s,"
+        " \"sev-support-es\": %s,"
+        " \"sev-support-snp\": %s"
+        " }"
+        " }\n",
+        cbitpos,
+        reduced_phys_bits,
+        sev_support ? "true" : "false",
+        sev_es_support ? "true" : "false",
+        sev_snp_support ? "true" : "false"
     );
     if (ret < 0) {
-	printf("Error writing to file %s: %s\n", path, strerror(errno));
+        printf("Error writing to file %s: %s\n", path, strerror(errno));
     }
 
     ret = fclose(file);
     if (ret != 0) {
-	printf("Error closing file %s: %s\n", path, strerror(errno));
+        printf("Error closing file %s: %s\n", path, strerror(errno));
     }
 
     return 0;
