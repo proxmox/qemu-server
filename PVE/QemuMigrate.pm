898e9296445ef2cdfea23e467cdfa9981b7c4607
Thomas Lamprecht <t.lamprecht@proxmox.com>
2024-11-20 11:55:00 +0100

    migrate: drop outdated PVE 7.2 check guarding cloudinit config section
    This check was added to guard the config format migration to a
    dedicated section for cloudinit. The respective package version set
    required for that to be understood is guaranteed to be available with
    pve-manager 7.2-13 or newer, as that raised the versioned dependencies
    respectively.
    
    This hedges against a migration from a node with newer version to one
    with older version, the effects would be basically that the name
    argument in a cloudinit section would override the current one, as the
    old parser interprets it as belonging to the main section, not the
    cloudinit section.
    
    We normally are cautious with removing such guards, and communicate
    stricter requirements than we check, to safeguard users with a certain
    ignorance or willingness to care for proper and periodic timely
    upgrades.
    
    But due to:
    
    - PVE 7 being EOL since a few months
    - PVE 7.2 being EOL for well over a year
    - the documented requirement to upgrade to latest PVE 7.4 before an
      upgrade to PVE 8
    - The relatively harmless effects when this check is voided
    
    we can drop that check more than safely now.
    
    Reported-by: Christian Ebner <c.ebner@proxmox.com>
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- PVE/QemuMigrate.pm
+++ PVE/QemuMigrate.pm
@@ -183,14 +183,6 @@ sub prepare {
     my $version = PVE::QemuServer::Helpers::get_node_pvecfg_version($self->{node});
     my $cloudinit_config = $conf->{cloudinit};
 
-    if (
-	PVE::QemuConfig->has_cloudinit($conf) && defined($cloudinit_config)
-	&& scalar(keys %$cloudinit_config) > 0
-	&& !PVE::QemuServer::Helpers::pvecfg_min_version($version, 7, 2, 13)
-    ) {
-	die "target node is too old (manager <= 7.2-13) and doesn't support new cloudinit section\n";
-    }
-
     my $repl_conf = PVE::ReplicationConfig->new();
     $self->{replication_jobcfg} = $repl_conf->find_local_replication_job($vmid, $self->{node});
     $self->{is_replicated} = $repl_conf->check_for_existing_jobs($vmid, 1);
