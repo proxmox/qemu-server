proxmox/qemu-server

commit c8ed1ac24ef915fb832ec1770ce252c72b730480

api: create disks: live import: use format from storage layer
Avoid that file_size_info() does auto-detection via qemu-img.

This also adapts to the new argument order.

Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

PVE/API2/Qemu.pm
@@ -438,9 +438,9 @@ my sub create_disks : prototype($$$$$$$$$$$) {
 		my ($source_storage, $source_volid) = PVE::Storage::parse_volume_id($source, 1);
 
 		if ($source_storage) { # PVE-managed volume
-		    my ($vtype, undef, undef, undef, undef, undef, $fmt)
-			= PVE::Storage::parse_volname($storecfg, $source);
-		    my $needs_extraction = PVE::QemuServer::Helpers::needs_extraction($vtype, $fmt);
+		    my ($vtype, $source_format) = (checked_parse_volname($storecfg, $source))[0,6];
+		    my $needs_extraction =
+			PVE::QemuServer::Helpers::needs_extraction($vtype, $source_format);
 		    my $untrusted = $vtype eq 'import' ? 1 : 0;
 		    if ($needs_extraction) {
 			print "extracting $source\n";
@@ -460,7 +460,7 @@ my sub create_disks : prototype($$$$$$$$$$$) {
 			my $path = PVE::Storage::path($storecfg, $source)
 			    or die "failed to get a path for '$source'\n";
 			# check potentially untrusted image file for import vtype
-			($size, my $source_format) = PVE::Storage::file_size_info($path, undef, $untrusted);
+			$size = PVE::Storage::file_size_info($path, undef, $source_format, $untrusted);
 			die "could not get file size of $path\n" if !$size;
 			$live_import_mapping->{$ds} = {
 			    path => $path,

commit 0fc03fc18d05170331b5ee263687e040f428992f

api: fix ova live import by using correct format for source image
Commit c8ed1ac2 ("api: create disks: live import: use format from
storage layer") broke live import from an OVA containing a disk,
because a combined format like 'ova+vmdk' was used for the live-import
disk mapping, leading to failure:

> invalid format 'ova+vmdk' for 'scsi0' mapping

There was also an informational message about the confusion earlier:

> file_size_info: '/mnt/pve/dir/images/135/vm-135-disk-0.vmdk': \
> falling back to 'raw' from unknown format 'ova+vmdk

Fixes: c8ed1ac2 ("api: create disks: live import: use format from storage layer")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

PVE/API2/Qemu.pm
@@ -29,7 +29,7 @@ use PVE::QemuConfig;
 use PVE::QemuServer;
 use PVE::QemuServer::Cloudinit;
 use PVE::QemuServer::CPUConfig;
-use PVE::QemuServer::Drive qw(checked_parse_volname);
+use PVE::QemuServer::Drive qw(checked_volume_format checked_parse_volname);
 use PVE::QemuServer::Helpers;
 use PVE::QemuServer::ImportDisk;
 use PVE::QemuServer::Monitor qw(mon_cmd);
@@ -450,6 +450,7 @@ my sub create_disks : prototype($$$$$$$$$$$) {
 			print "finished extracting to $extracted_volid\n";
 			push @$vollist, $extracted_volid;
 			$source = $extracted_volid;
+			$source_format = checked_volume_format($storecfg, $source);
 
 			my (undef, undef, undef, $parent)
 			    = PVE::Storage::volume_size_info($storecfg, $source);