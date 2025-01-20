proxmox/qemu-server

commit 919e69d0d3ddd5ccf35fe71cad39cf860bfb5337

machine: add check_and_pin_machine_string() helper
Extract the logic for guest OS-type dependent machine version pinning
into a dedicated helper, so it can be re-used.

Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

PVE/API2/Qemu.pm
@@ -1242,17 +1242,9 @@ __PACKAGE__->register_method({
 			$conf->{vmgenid} = PVE::QemuServer::generate_uuid();
 		    }
 
-		    my $machine_conf = PVE::QemuServer::Machine::parse_machine($conf->{machine});
-		    my $machine = $machine_conf->{type};
-		    if (!$machine || $machine =~ m/^(?:pc|q35|virt)$/) {
-			# always pin Windows' machine version on create, they get to easily confused
-			if (PVE::QemuServer::Helpers::windows_version($conf->{ostype})) {
-			    $machine_conf->{type} =
-				PVE::QemuServer::Machine::windows_get_pinned_machine_version($machine);
-			    $conf->{machine} = PVE::QemuServer::Machine::print_machine($machine_conf);
-			}
-		    }
-		    PVE::QemuServer::Machine::assert_valid_machine_property($machine_conf);
+		    # always pin Windows' machine version on create, they get confused too easily
+		    $conf->{machine} = PVE::QemuServer::Machine::check_and_pin_machine_string(
+			$conf->{machine}, $conf->{ostype});
 
 		    $conf->{lock} = 'import' if $live_import_mapping;
 

PVE/QemuServer/Machine.pm
@@ -261,4 +261,20 @@ sub get_vm_machine {
     return $machine;
 }
 
+sub check_and_pin_machine_string {
+    my ($machine_string, $ostype) = @_;
+
+    my $machine_conf = parse_machine($machine_string);
+    my $machine = $machine_conf->{type};
+    if (!$machine || $machine =~ m/^(?:pc|q35|virt)$/) {
+	# always pin Windows' machine version on create, they get confused too easily
+	if (PVE::QemuServer::Helpers::windows_version($ostype)) {
+	    $machine_conf->{type} = windows_get_pinned_machine_version($machine);
+	}
+    }
+
+    assert_valid_machine_property($machine_conf);
+    return print_machine($machine_conf);
+}
+
 1;

commit ae1da616a73cca31266cc03f45fe852cb69e008b

api: create/update vm: avoid printing empty machine string
While no problem is known with having an empty machine string in the
configuration and it was already possible setting an empty machine
manually via 'qm set', the behavior changed because of commit 919e69d0
("machine: add check_and_pin_machine_string() helper") and there is
potential for problematic edge cases. Restore the previous behavior.

Pinning is also required when there is no given machine type, so the
call to check_and_pin_machine_string() should stay unconditional.

For update, pinning was recently added by commit 7a9570f3 ("api:
update vm config: pin machine version when switching to windows os
type"), so bring that in-line with the behavior for create too.

Another idea would've been to just return the default machine in
check_and_pin_machine_string(), but that would also be a change in
behavior. In particular, the default depends on the arch, so an empty
machine type will pick the default machine for the currently
configured arch even when the arch is later changed.

Reported-by: Daniel Herzig <d.herzig@proxmox.com>
Fixes: 919e69d0 ("machine: add check_and_pin_machine_string() helper")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

PVE/API2/Qemu.pm
@@ -1243,8 +1243,9 @@ __PACKAGE__->register_method({
 		    }
 
 		    # always pin Windows' machine version on create, they get confused too easily
-		    $conf->{machine} = PVE::QemuServer::Machine::check_and_pin_machine_string(
+		    my $machine_string = PVE::QemuServer::Machine::check_and_pin_machine_string(
 			$conf->{machine}, $conf->{ostype});
+		    $conf->{machine} = $machine_string if $machine_string;
 
 		    $conf->{lock} = 'import' if $live_import_mapping;
 
@@ -2120,9 +2121,10 @@ my $update_vm_api  = sub {
 			&& !$modified->{machine} # detects deletion
 		    ) {
 			eval {
-			    $conf->{pending}->{machine} =
+			    my $machine_string =
 				PVE::QemuServer::Machine::check_and_pin_machine_string(
 				    $conf->{machine}, $param->{ostype});
+			    $conf->{pending}->{machine} = $machine_string if $machine_string;
 			};
 			print "automatic pinning of machine version failed - $@" if $@;
 		    }