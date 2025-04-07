proxmox/qemu-server

commit 8107b378c996e498c5ed775beadea48295101f8b

add serial:1 to vmstatus when config has a serial device configured
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Reviewed-by: Thomas Lamprecht <t.lamprecht@proxmox.com>
Tested-by: Thomas Lamprecht <t.lamprecht@proxmox.com>
Acked-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

PVE/QemuServer.pm
@@ -2668,6 +2668,8 @@ sub vmstatus {
 
         $d->{template} = PVE::QemuConfig->is_template($conf);
 
+	$d->{serial} = 1 if conf_has_serial($conf);
+
 	$res->{$vmid} = $d;
     }
 
@@ -2860,6 +2862,18 @@ sub foreach_volid {
     foreach my $volid (keys %$volhash) {
 	&$func($volid, $volhash->{$volid}, @param);
     }
+}
+
+sub conf_has_serial {
+    my ($conf) = @_;
+
+    for (my $i = 0; $i < $MAX_SERIAL_PORTS; $i++)  {
+	if ($conf->{"serial$i"}) {
+	    return 1;
+	}
+    }
+
+    return 0;
 }
 
 sub vga_conf_has_spice {

commit 28d8e248c31128e1585169e3435b66e2b6ff225e

vmstatus_return_properties: add missing serial property
Fixes: 8107b37 ("add serial:1 to vmstatus when config has a serial device configured")
Signed-off-by: Aaron Lauterer <a.lauterer@proxmox.com>
Link: https://lore.proxmox.com/20250407162718.495812-2-a.lauterer@proxmox.com/

PVE/QemuServer.pm
@@ -2789,6 +2789,11 @@ our $vmstatus_return_properties = {
 	optional => 1,
 	default => 0,
     },
+    serial => {
+	description => "Guest has serial device configured.",
+	type => 'boolean',
+	optional => 1,
+    },
 };
 
 my $last_proc_pid_stat;