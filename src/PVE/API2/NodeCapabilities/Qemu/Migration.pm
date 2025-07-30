ef1fd2e2aac453c062d54d89bcfbfd40bcd06618
Christoph Heiss <c.heiss@proxmox.com>
2025-07-30 11:45:39 +0200

    api2: qemu: add module exposing node migration capabilities
    Similar to the already existing ones for CPU and QEMU machine support.
    
    Very simple for now, only provides one property for now:
    
      'has-dbus-vmstate' - Whether the dbus-vmstate is available/installed
    
    Tested-by: Stefan Hanreich <s.hanreich@proxmox.com>
    Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>
    Link: https://lore.proxmox.com/20250730094549.263805-6-c.heiss@proxmox.com

---
+++ src/PVE/API2/NodeCapabilities/Qemu/Migration.pm
@@ -0,0 +1,48 @@
+package PVE::API2::NodeCapabilities::Qemu::Migration;
+
+use strict;
+use warnings;
+
+use JSON;
+use PVE::JSONSchema qw(get_standard_option);
+use PVE::RESTHandler;
+
+use base qw(PVE::RESTHandler);
+
+__PACKAGE__->register_method({
+    name => 'capabilities',
+    path => '',
+    method => 'GET',
+    proxyto => 'node',
+    description => 'Get node-specific QEMU migration capabilities of the node.'
+        . " Requires the 'Sys.Audit' permission on '/nodes/<node>'.",
+    permissions => {
+        check => ['perm', '/nodes/{node}', ['Sys.Audit']],
+    },
+    parameters => {
+        additionalProperties => 0,
+        properties => {
+            node => get_standard_option('pve-node'),
+        },
+    },
+    returns => {
+        type => 'object',
+        additionalProperties => 0,
+        properties => {
+            'dbus-vmstate' => {
+                type => 'boolean',
+                description => 'Whether the host supports live-migrating additional'
+                    . ' VM state via the dbus-vmstate helper.',
+            },
+        },
+    },
+    code => sub {
+        return {
+            'has-dbus-vmstate' => -f '/usr/libexec/qemu-server/dbus-vmstate'
+            ? JSON::true
+            : JSON::false,
+        };
+    },
+});
+
+1;
