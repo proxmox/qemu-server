proxmox/qemu-server

commit 067a0f55bbf155936861d8844c91dfc16b883c1d

vmstate: improve cleaning up dbus-vmstate and avoid spurious warning
First, moving to vm_stop_cleanup(), which is a better fit for this.
It gets called by the cleanup API method in case of unclean shutdown or
from inside the guest.

In every case, the dbus-vmstate daemon should _never_ be running at this
point, as it is started only before migration and stopped directly after
migration, before vm_stop_cleanup() is even called. So it should only be
left running in case of a crash during migration.

Calling it anyway here ensures that the daemon is always (cleanly) shut
down. As the dbus-vmstate is part of the VM scope unit, that would it
tear it down too as a last resort.

Fixes the following spurious warning when a VM was shutdown from inside
the guest:

  `failed to retrieve org.qemu.VMState1 owners: org.freedesktop.DBus.Error.NameHasNoOwner: Could not get owners of name 'org.qemu.VMState1': no such name`

Reported-by: Hannes Duerr <h.duerr@proxmox.com>
Reported-by: Maximiliano Sandoval <m.sandoval@proxmox.com>
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>
Link: https://lore.proxmox.com/20250804133002.1625925-1-c.heiss@proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/QemuServer.pm
@@ -6102,6 +6102,8 @@ sub vm_stop_cleanup {
     my ($storecfg, $vmid, $conf, $keepActive, $apply_pending_changes, $noerr) = @_;
 
     eval {
+        # ensure that no dbus-vmstate helper is left running in any case
+        PVE::QemuServer::DBusVMState::qemu_del_dbus_vmstate($vmid, quiet => $noerr);
 
         if (!$keepActive) {
             my $vollist = get_vm_volumes($conf);

src/PVE/CLI/qm.pm
@@ -1056,9 +1056,6 @@ __PACKAGE__->register_method({
                     PVE::QemuServer::vm_stop_cleanup($storecfg, $vmid, $conf, 0, 0, 1);
                 }
 
-                # ensure that no dbus-vmstate helper is left running in any case
-                PVE::QemuServer::DBusVMState::qemu_del_dbus_vmstate($vmid);
-
                 PVE::GuestHelpers::exec_hookscript($conf, $vmid, 'post-stop');
 
                 $restart = eval { PVE::QemuServer::clear_reboot_request($vmid) };

commit 6df5858a1cbfaa87e476d4b2c1dead8e45b5caca

vmstate: always quiesce warnings on vm stop cleanup
At this point, the dbus-vmstate helper is not expected to be running
anymore.

Using $noerr here didn't really make sense - as it never should be
running anymore at this point, plus the VM should also be stopped - thus
the "happy" path here is to fail removing the dbus-vmstate helper.
It resulted in another spurious warning _after_ a migration on the
source node.

Fixes: 067a0f55 ("vmstate: improve cleaning up dbus-vmstate and avoid spurious warning")
Reported-by: Friedrich Weber <f.weber@proxmox.com>
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>
Link: https://lore.proxmox.com/20250805095828.301188-1-c.heiss@proxmox.com

src/PVE/QemuServer.pm
@@ -6103,7 +6103,8 @@ sub vm_stop_cleanup {
 
     eval {
         # ensure that no dbus-vmstate helper is left running in any case
-        PVE::QemuServer::DBusVMState::qemu_del_dbus_vmstate($vmid, quiet => $noerr);
+        # at this point, it should never be still running, so quiesce any warnings
+        PVE::QemuServer::DBusVMState::qemu_del_dbus_vmstate($vmid, quiet => 1);
 
         if (!$keepActive) {
             my $vollist = get_vm_volumes($conf);