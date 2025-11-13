proxmox/qemu-server

commit 3a4e0144744aca1d961864fb76ae84cfd70c8f7d

cfg2cmd: turn off hpet for Linux VMs running at least kernel 2.6 and machine type >= 10.1
Recent enough Linux versions already use 'kvm-clock' rather than
'hpet' as the default clock source [0][1]. Changes in QEMU [2] led to
slightly increased CPU usage when using hpet [3][4]:

> the timer must be kept running even if not enabled, in
> order to set the ISR flag, so writes to HPET_TN_CFG must
> not call hpet_del_timer()

Upstream suggested to not use hpet if possible [5][6]:

> That said, if you can disable the HPET timer by default without
> problems with e.g. live migration I strongly suggest you do. And in
> the mean time you can also revert these patches, they were actually
> reported as bugs but it's not clear what guest OS was affected.

> No, the bug reports are really just for corner cases and there are no
> huge issues. However, both Linux and Windows give the HPET a
> relatively high priority that it probably does not deserve. :)

There were more changes in QEMU, so it would require more reverts.
Thus, disable the timer. People having a Linux VM pinned to an older
machine version or using other os types will see the increased usage
again if installing the new QEMU 10.1 binary, but that seems like a
fair trade-off for reducing CPU load for everybody else and being able
to move forward.

The is_linux() helper does not include the 'l24' os type by default,
because all except one existing checks as well as the newly introduced
check are specifically for 'l26' and most future features are not
worth considering for 'l24' either.

Users of Linux 2.6.x before v2.6.26 might need to pin the machine
version or manually enable hpet if they want to continue using HPET.
Otherwise, there is acpi_pm since v2.6.18 that should be automatically
picked.

[0]: /sys/devices/system/clocksource/clocksource0/current_clocksource
[1]: Kernel commit 790c73f6289a ("x86: KVM guest: paravirtualized clocksource") in v2.6.26+

src/PVE/QemuServer.pm
@@ -3382,7 +3382,7 @@ sub config_to_command {
     }
 
     # For now, handles only specific parts, but the final goal is to cover everything.
-    my $cfg2cmd = PVE::QemuServer::Cfg2Cmd->new($conf, $defaults);
+    my $cfg2cmd = PVE::QemuServer::Cfg2Cmd->new($conf, $defaults, $version_guard);
     my $generated = $cfg2cmd->generate();
     push $cmd->@*, '-global', $_ for ($generated->global_flags() // [])->@*;
     push $machineFlags->@*, ($generated->machine_flags() // [])->@*;

src/PVE/QemuServer/Cfg2Cmd.pm
@@ -7,15 +7,16 @@ use PVE::QemuServer::Cfg2Cmd::Timer;
 use PVE::QemuServer::Helpers;
 
 sub new {
-    my ($class, $conf, $defaults) = @_;
+    my ($class, $conf, $defaults, $version_guard) = @_;
 
     my $self = bless {
         conf => $conf,
         defaults => $defaults,
+        'version-guard' => $version_guard,
     }, $class;
 
-    my $ostype = $self->get_prop('ostype');
-    $self->{'windows-version'} = PVE::QemuServer::Helpers::windows_version($ostype);
+    $self->{ostype} = $self->get_prop('ostype');
+    $self->{'windows-version'} = PVE::QemuServer::Helpers::windows_version($self->{ostype});
 
     return $self;
 }
@@ -74,10 +75,33 @@ sub rtc_flags {
     return $self->{'rtc-flags'};
 }
 
+=head3 is_linux
+
+    if ($self->is_linux()) {
+        do_something_for_linux_vms();
+    }
+
+Check if the virtual machine is configured for running Linux. Does not include the C<l24> os type
+by default. Specify C<$include_l24> if that is desired.
+
+=cut
+
+sub is_linux {
+    my ($self, $include_l24) = @_;
+
+    return $self->{ostype} eq 'l26' || ($include_l24 && $self->{ostype} eq 'l24');
+}
+
 sub windows_version {
     my ($self) = @_;
 
     return $self->{'windows-version'};
+}
+
+sub version_guard {
+    my ($self, $major, $minor, $pve) = @_;
+
+    $self->{'version-guard'}->($major, $minor, $pve);
 }
 
 sub generate {

src/PVE/QemuServer/Cfg2Cmd/Timer.pm
@@ -21,6 +21,8 @@ sub generate {
     if ($cfg2cmd->windows_version() >= 6) {
         $cfg2cmd->add_global_flag('kvm-pit.lost_tick_policy=discard');
         $cfg2cmd->add_machine_flag('hpet=off');
+    } elsif ($cfg2cmd->is_linux() && $cfg2cmd->version_guard(10, 1, 0)) {
+        $cfg2cmd->add_machine_flag('hpet=off');
     }
 
     $cfg2cmd->add_rtc_flag('driftfix=slew') if $time_drift_fix;

commit 1de4998ed02ab42f9084c54ee40f7e039c0e8cb9

fix #7014: fix regression starting aarch64 VMs with machine type >= 10.1
The 'virt' machine type used for 'aarch64' does not support the 'hpet'
machine flag. Change add_machine_flag() to only add the flag when the
current machine type supports it and rename the method to
add_machine_flag_if_supported() to make the behavior obvious from the
name. If a future caller really depends on adding a specific flag and
would rather die when the machine type does not support it, it can
check the return value whether the flag was successfully added or not.

Fixes: 3a4e0144 ("cfg2cmd: turn off hpet for Linux VMs running at least kernel 2.6 and machine type >= 10.1")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Link: https://lore.proxmox.com/20251112112959.237909-4-f.ebner@proxmox.com

src/PVE/QemuServer.pm
@@ -3401,7 +3401,8 @@ sub config_to_command {
     }
 
     # For now, handles only specific parts, but the final goal is to cover everything.
-    my $cfg2cmd = PVE::QemuServer::Cfg2Cmd->new($conf, $defaults, $version_guard);
+    my $cfg2cmd_opts = { forcemachine => $forcemachine };
+    my $cfg2cmd = PVE::QemuServer::Cfg2Cmd->new($conf, $defaults, $version_guard, $cfg2cmd_opts);
     my $generated = $cfg2cmd->generate();
     push $cmd->@*, '-global', $_ for ($generated->global_flags() // [])->@*;
     push $machineFlags->@*, ($generated->machine_flags() // [])->@*;

src/PVE/QemuServer/Cfg2Cmd.pm
@@ -5,9 +5,10 @@ use strict;
 
 use PVE::QemuServer::Cfg2Cmd::Timer;
 use PVE::QemuServer::Helpers;
+use PVE::QemuServer::Machine;
 
 sub new {
-    my ($class, $conf, $defaults, $version_guard) = @_;
+    my ($class, $conf, $defaults, $version_guard, $opts) = @_;
 
     my $self = bless {
         conf => $conf,
@@ -17,6 +18,10 @@ sub new {
 
     $self->{ostype} = $self->get_prop('ostype');
     $self->{'windows-version'} = PVE::QemuServer::Helpers::windows_version($self->{ostype});
+
+    my $arch = PVE::QemuServer::Helpers::get_vm_arch($conf);
+    $self->{'machine-type'} =
+        PVE::QemuServer::Machine::get_vm_machine($conf, $opts->{forcemachine}, $arch);
 
     return $self;
 }
@@ -51,10 +56,23 @@ sub global_flags {
     return $self->{'global-flags'};
 }
 
-sub add_machine_flag {
-    my ($self, $flag) = @_;
+=head3 add_machine_flag_if_supported
 
-    push $self->{'machine-flags'}->@*, $flag;
+    my $success = $self->add_machine_flag_if_supported($flag_name, $value);
+
+Add flag C<$flag_name> with value C<$value> to the machine flags if the current machine type
+supports it. Returns whether the flag was added or not.
+
+=cut
+
+sub add_machine_flag_if_supported {
+    my ($self, $flag_name, $value) = @_;
+
+    return if !PVE::QemuServer::Machine::machine_supports_flag($self->{'machine-type'}, $flag_name);
+
+    push $self->{'machine-flags'}->@*, "${flag_name}=${value}";
+
+    return 1;
 }
 
 sub machine_flags {

src/PVE/QemuServer/Cfg2Cmd/Timer.pm
@@ -20,9 +20,9 @@ sub generate {
 
     if ($cfg2cmd->windows_version() >= 6) {
         $cfg2cmd->add_global_flag('kvm-pit.lost_tick_policy=discard');
-        $cfg2cmd->add_machine_flag('hpet=off');
+        $cfg2cmd->add_machine_flag_if_supported('hpet', 'off');
     } elsif ($cfg2cmd->is_linux() && $cfg2cmd->version_guard(10, 1, 0)) {
-        $cfg2cmd->add_machine_flag('hpet=off');
+        $cfg2cmd->add_machine_flag_if_supported('hpet', 'off');
     }
 
     $cfg2cmd->add_rtc_flag('driftfix=slew') if $time_drift_fix;