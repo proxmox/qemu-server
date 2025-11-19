0fd8eb7db092087c6b0b17cbbcaab880276f60c1
Fiona Ebner <f.ebner@proxmox.com>
2025-11-19 10:59:58 +0100

    cpu config: nested-virt: always recommend a CPU model similar to the host
    Not only for Windows, nested virtualization with just x86-64-v2-AES
    and the nested-virt flag does not work on Linux either. While
    virtualization support is detected, actually starting a nested VM with
    such a configuration fails. For example, a Proxmox VE/PBS file-restore
    VM on my test system fails with the following (adding the apic and
    x2apic flags on top wouldn't make a difference):
    
    [2025-11-19T10:37:44+01:00] PBS file restore VM log
    ERROR: Unable to locate IOAPIC for GSI 2
    ERROR: Unable to locate IOAPIC for GSI 5
    ERROR: Unable to locate IOAPIC for GSI 9
    ERROR: Unable to locate IOAPIC for GSI 10
    ERROR: Unable to locate IOAPIC for GSI 11
    ERROR: Unable to locate IOAPIC for GSI 1
    ERROR: Unable to locate IOAPIC for GSI 2
    ERROR: Unable to locate IOAPIC for GSI 3
    ERROR: Unable to locate IOAPIC for GSI 4
    ERROR: Unable to locate IOAPIC for GSI 5
    ERROR: Unable to locate IOAPIC for GSI 6
    ERROR: Unable to locate IOAPIC for GSI 7
    ERROR: Unable to locate IOAPIC for GSI 8
    ERROR: Unable to locate IOAPIC for GSI 9
    ERROR: Unable to locate IOAPIC for GSI 10
    ERROR: Unable to locate IOAPIC for GSI 11
    ERROR: Unable to locate IOAPIC for GSI 12
    ERROR: Unable to locate IOAPIC for GSI 13
    ERROR: Unable to locate IOAPIC for GSI 14
    ERROR: Unable to locate IOAPIC for GSI 15
    ..MP-BIOS bug: 8254 timer not connected to IO-APIC
    Kernel panic - not syncing: IO-APIC + timer doesn't work!  Boot with apic=debug and send a report.  Then try booting with the 'noapic' option.
    CPU: 0 UID: 0 PID: 0 Comm: swapper/0 Tainted: G        W          6.14.6-pbs-restore #1
    Tainted: [W]=WARN
    Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS rel-1.17.0-0-gb52ca86e094d-prebuilt.qemu.org 04/01/2014
    Call Trace:
     <TASK>
     dump_stack_lvl+0x4a/0x70
     panic+0xf5/0x281
     ? _printk+0x55/0x60
     setup_IO_APIC+0x6ee/0x6f0
     ? __ioapic_read_entry+0x23/0x40
     ? clear_IO_APIC_pin+0x11e/0x1e0
     apic_intr_mode_init+0xad/0xc0
     x86_late_time_init+0x12/0x30
     start_kernel+0x538/0x5b0
     x86_64_start_reservations+0x25/0x30
     x86_64_start_kernel+0x73/0x80
     common_startup_64+0x129/0x138
     </TASK>
    
    Reported-by: Lukas Wagner <l.wagner@proxmox.com>
    Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
    Link: https://lore.proxmox.com/20251119100144.33035-1-f.ebner@proxmox.com

--- src/PVE/QemuServer/CPUConfig.pm
+++ src/PVE/QemuServer/CPUConfig.pm
@@ -170,7 +170,7 @@ my $supported_cpu_flags = [
         name => 'nested-virt',
         description => "Controls nested virtualization, namely 'svm' for AMD CPUs and 'vmx' for"
             . " Intel CPUs. Live migration still only works if it's the same flag on both sides."
-            . " For Windows, use a CPU model similar to the host, with the same vendor!",
+            . " Use a CPU model similar to the host, with the same vendor, not x86-64-vX!",
     },
     {
         name => 'md-clear',
