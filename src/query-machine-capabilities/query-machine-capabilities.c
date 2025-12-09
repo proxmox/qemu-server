proxmox/qemu-server

commit c1934a5de9419216ac6cb9d87a5d2b086605af1b

query machine capabilities: only query features of matching CPU vendor
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

src/query-machine-capabilities/query-machine-capabilities.c
@@ -26,6 +26,16 @@ typedef struct {
     bool tdx_support;
 } cpu_caps_intel_tdx_t;
 
+static inline void cpu_vendor(char vendor[13]) {
+    uint32_t eax;
+    uint32_t *vp = (uint32_t *)vendor;
+    asm volatile("cpuid"
+        : "=a"(eax), "=b"(*vp), "=c"(*(vp+2)), "=d"(*(vp+1))
+        : "a"(0)
+    );
+    vendor[12] = '\0';
+}
+
 int read_msr(uint32_t msr_index, uint64_t *value) {
     uint64_t data;
     char* msr_file_name = "/dev/cpu/0/msr";
@@ -119,45 +129,54 @@ int main() {
         return 1;
     }
 
-    cpu_caps_amd_sev_t caps_sev;
-    query_cpu_capabilities_sev(&caps_sev);
-
     FILE *file = fopen(OUTPUT_PATH, "w");
     if (file == NULL) {
         eprintf("Error opening to file '" OUTPUT_PATH "': %s\n", strerror(errno));
         return 1;
     }
 
-    int ret = fprintf(file,
-        "{"
-        " \"amd-sev\": {"
-        " \"cbitpos\": %u,"
-        " \"reduced-phys-bits\": %u,"
-        " \"sev-support\": %s,"
-        " \"sev-support-es\": %s,"
-        " \"sev-support-snp\": %s"
-        " }",
-        caps_sev.cbitpos,
-        caps_sev.reduced_phys_bits,
-        caps_sev.sev_support ? "true" : "false",
-        caps_sev.sev_es_support ? "true" : "false",
-        caps_sev.sev_snp_support ? "true" : "false"
-    );
+    char vendor[13];
+    cpu_vendor(vendor);
+    printf("%s\n", vendor);
+
+    int ret = fprintf(file, "{");
     if (ret < 0) {
         eprintf("Error writing to file '" OUTPUT_PATH "': %s\n", strerror(errno));
     }
 
-    cpu_caps_intel_tdx_t caps_tdx;
-    if (query_cpu_capabilities_tdx(&caps_tdx) == 0) {
+    if (strncmp(vendor, "AuthenticAMD", 12) == 0) {
+        cpu_caps_amd_sev_t caps_sev;
+        query_cpu_capabilities_sev(&caps_sev);
+
         ret = fprintf(file,
-            ","
-            " \"intel-tdx\": {"
-            " \"tdx-support\": %s"
+            " \"amd-sev\": {"
+            " \"cbitpos\": %u,"
+            " \"reduced-phys-bits\": %u,"
+            " \"sev-support\": %s,"
+            " \"sev-support-es\": %s,"
+            " \"sev-support-snp\": %s"
             " }",
-            caps_tdx.tdx_support ? "true" : "false"
+            caps_sev.cbitpos,
+            caps_sev.reduced_phys_bits,
+            caps_sev.sev_support ? "true" : "false",
+            caps_sev.sev_es_support ? "true" : "false",
+            caps_sev.sev_snp_support ? "true" : "false"
         );
         if (ret < 0) {
             eprintf("Error writing to file '" OUTPUT_PATH "': %s\n", strerror(errno));
+        }
+    } else if (strncmp(vendor, "GenuineIntel", 12) == 0) {
+        cpu_caps_intel_tdx_t caps_tdx;
+        if (query_cpu_capabilities_tdx(&caps_tdx) == 0) {
+            ret = fprintf(file,
+                " \"intel-tdx\": {"
+                " \"tdx-support\": %s"
+                " }",
+                caps_tdx.tdx_support ? "true" : "false"
+            );
+            if (ret < 0) {
+                eprintf("Error writing to file '" OUTPUT_PATH "': %s\n", strerror(errno));
+            }
         }
     }
 

commit 6ae6a468f1eb137aaf677a53fb6c78ca9bba87ca

query machine capabilities: remove left-over debug print
This is a left-over debug print that was accidentally committed.

Fixes: c1934a5d ("query machine capabilities: only query features of matching CPU vendor")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

src/query-machine-capabilities/query-machine-capabilities.c
@@ -137,7 +137,6 @@ int main() {
 
     char vendor[13];
     cpu_vendor(vendor);
-    printf("%s\n", vendor);
 
     int ret = fprintf(file, "{");
     if (ret < 0) {