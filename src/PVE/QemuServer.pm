proxmox/qemu-server

commit 0db14e60109ea91a558f0f08594583588ef9ec66

vm start: remove left-over VM-state-related properties
When cloning from a snapshot, VM-state-related properties would be
accidentally copied, see commit "partially fix #6805: api: clone:
properly remove all snapshot-related info". Detect and remove such
left-over properties upon VM start.

Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Link: https://lore.proxmox.com/20250917163038.488710-7-f.ebner@proxmox.com

src/PVE/QemuServer.pm
@@ -5553,6 +5553,20 @@ sub vm_migrate_alloc_nbd_disks {
     return $nbd;
 }
 
+my sub remove_left_over_vmstate_opts {
+    my ($vmid, $conf) = @_;
+
+    my $found;
+    for my $opt (qw(running-nets-host-mtu runningmachine runningcpu)) {
+        if (defined($conf->{$opt})) {
+            print "No VM state set, removing left-over option '$opt'\n";
+            delete $conf->{$opt};
+            $found = 1;
+        }
+    }
+    PVE::QemuConfig->write_config($vmid, $conf) if $found;
+}
+
 # see vm_start_nolock for parameters, additionally:
 # migrate_opts:
 #   storagemap = parsed storage map for allocating NBD disks
@@ -6039,6 +6053,8 @@ sub vm_start_nolock {
         }
         delete $conf->@{qw(lock vmstate running-nets-host-mtu runningmachine runningcpu)};
         PVE::QemuConfig->write_config($vmid, $conf);
+    } elsif (!$conf->{vmstate}) {
+        remove_left_over_vmstate_opts($vmid, $conf);
     }
 
     PVE::GuestHelpers::exec_hookscript($conf, $vmid, 'post-start');

commit be61afa1c180c0e415245fc48489dfdb216153fd

migration: vm start: avoid config write when there are left-over VM-state-related properties
The remove_left_over_vmstate_opts() function might write the
configuration, so it cannot be called in the context of a migration,
where the configuration file will be moved over from the source node
later. As reported in the enterprise support, this could cause an
error like:
> close (rename) atomic file '/etc/pve/nodes/XYZ/qemu-server/211.conf' failed: File exists

Fixes: 0db14e60 ("vm start: remove left-over VM-state-related properties")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Link: https://lore.proxmox.com/all/20260102131852.107940-1-f.ebner@proxmox.com

src/PVE/QemuServer.pm
@@ -5932,7 +5932,7 @@ sub vm_start_nolock {
         }
         delete $conf->@{qw(lock vmstate running-nets-host-mtu runningmachine runningcpu)};
         PVE::QemuConfig->write_config($vmid, $conf);
-    } elsif (!$conf->{vmstate}) {
+    } elsif (!$conf->{vmstate} && !$migratedfrom) {
         remove_left_over_vmstate_opts($vmid, $conf);
     }
 