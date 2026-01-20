proxmox/qemu-server

commit 95eb95c3e6c7cb4f8427a8df65e360a7b67fb465

qm enroll-efi-keys: move potential blocking operation out of lock
Prepare the efidisk first outside of any config locking, then lock the
config, compare the digest to protect against concurrent modifications
and then commit the new efidisk.

That way the config-locked section is kept to a minimum and has no
operations doing disk IO inside anymore.

While at it try to do a best-effort clean-up of the new volume if
there where any concurrent changes.

Suggested-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/CLI/qm.pm
@@ -714,36 +714,51 @@ __PACKAGE__->register_method({
 
         my $vmid = extract_param($param, 'vmid');
 
-        my $enroll_fn = sub {
-            my $conf = PVE::QemuConfig->load_config($vmid);
+        my $conf = PVE::QemuConfig->load_config($vmid);
+        PVE::QemuConfig->check_lock($conf);
 
-            PVE::QemuConfig->check_lock($conf);
-            die "VM $vmid is running\n" if PVE::QemuServer::Helpers::vm_running_locally($vmid);
-            die "VM $vmid is a template\n" if PVE::QemuConfig->is_template($conf);
-            die "VM $vmid has no EFI disk configured\n" if !$conf->{efidisk0};
+        die "VM $vmid is running\n" if PVE::QemuServer::Helpers::vm_running_locally($vmid);
+        die "VM $vmid is a template\n" if PVE::QemuConfig->is_template($conf);
+        die "VM $vmid has no EFI disk configured\n" if !$conf->{efidisk0};
 
-            my $ostype = $conf->{ostype};
-            if (!defined($ostype) || ($ostype ne 'win10' && $ostype ne 'win11')) {
-                print "skipping - OS type is neither Windows 10 nor Windows 11\n";
-                return;
-            }
-
-            my $storecfg = PVE::Storage::config();
-
-            my $updated = PVE::QemuServer::OVMF::ensure_ms_2023_cert_enrolled(
-                $storecfg, $vmid, $conf->{efidisk0},
-            );
-            if ($updated) {
-                $conf->{efidisk0} = $updated;
-                PVE::QemuConfig->write_config($vmid, $conf);
-            } else {
-                print "skipping - no pre-enrolled keys or already got ms-cert=2023 marker\n";
-            }
-
+        my $ostype = $conf->{ostype};
+        if (!defined($ostype) || ($ostype ne 'win10' && $ostype ne 'win11')) {
+            print "skipping - OS type is neither Windows 10 nor Windows 11\n";
             return;
-        };
+        }
 
-        PVE::QemuConfig->lock_config($vmid, $enroll_fn);
+        my $storecfg = PVE::Storage::config();
+
+        my $updated = PVE::QemuServer::OVMF::ensure_ms_2023_cert_enrolled(
+            $storecfg, $vmid, $conf->{efidisk0},
+        );
+
+        if (!$updated) {
+            print "skipping - no pre-enrolled keys or already got ms-cert=2023 marker\n";
+            return;
+        }
+
+        PVE::QemuConfig->lock_config(
+            $vmid,
+            sub {
+                my $locked_conf = PVE::QemuConfig->load_config($vmid);
+
+                eval { PVE::Tools::assert_if_modified($conf->{digest}, $locked_conf->{digest}) };
+                if (my $err = $@) {
+                    eval {
+                        my $drive = PVE::QemuServer::Drive::parse_drive('efidisk0', $updated);
+                        PVE::Storage::vdisk_free($storecfg, $drive->{file});
+                    };
+                    warn "failed to clean-up prepared efidisk volume - $@" if $@;
+                    die "VM ${vmid}: $err";
+                }
+
+                $locked_conf->{efidisk0} = $updated;
+                PVE::QemuConfig->write_config($vmid, $locked_conf);
+                print "successfully updated efidisk\n";
+            },
+        );
+
         return;
     },
 });

commit 8a88aeee86f2465368725dd22e5b8f3c4b390e83

qm enroll-efi-keys: do not remove EFI disk when config was modified during operation
The EFI disk is already pre-existing and should not be removed in case
the VM configuration was modified during the enrollment operation.
It's not critical if the new certs are enrolled but the marker is not
written to the configuration. Worst case, the operation is just done
again, where virt-fw-vars will just skip enrollment after detecting
that the new certs are already on the disk.

Fixes: 95eb95c3 ("qm enroll-efi-keys: move potential blocking operation out of lock")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Link: https://lore.proxmox.com/20260113105440.68336-2-f.ebner@proxmox.com

src/PVE/CLI/qm.pm
@@ -744,14 +744,7 @@ __PACKAGE__->register_method({
                 my $locked_conf = PVE::QemuConfig->load_config($vmid);
 
                 eval { PVE::Tools::assert_if_modified($conf->{digest}, $locked_conf->{digest}) };
-                if (my $err = $@) {
-                    eval {
-                        my $drive = PVE::QemuServer::Drive::parse_drive('efidisk0', $updated);
-                        PVE::Storage::vdisk_free($storecfg, $drive->{file});
-                    };
-                    warn "failed to clean-up prepared efidisk volume - $@" if $@;
-                    die "VM ${vmid}: $err";
-                }
+                die "VM ${vmid}: $@" if $@;
 
                 $locked_conf->{efidisk0} = $updated;
                 PVE::QemuConfig->write_config($vmid, $locked_conf);