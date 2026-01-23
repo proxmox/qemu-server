proxmox/qemu-server

commit 551c0078de6c2986b75a5db589db881723207624

fix #6935: vmstatus: use CGroup for host memory usage
after a certain amount of KSM sharing, PSS lookups become prohibitively
expensive. instead of reverting to the old broken method, simply use the
cgroup's memory usage as `memhost` value.

this does not account for merged pages because of KSM anymore.

I benchmarked this with 4 VMs running with different levels of KSM sharing. in
the output below, "merged pages" refers to the contents of
/proc/$pid/ksm_merging_pages, the extract_* benchmark runs refer to four
different variants of extracting memory usage, with the actual extraction part
running 1000x in a loop for each run to amortize perl/process setup costs,
qm_status_stock is `qm status $vmid --verbose`, and qm_status_pateched is `perl
-I./src/PVE ./src/bin/qm status $vmid --verbose` with this patch applied.

the variants:
- extract_pss: status before this patch, query smaps_rollup for each process
  that is part of the qemu.slice of the VM
- extract_rss: extract VmRSS from the `/proc/$pid/status` file of the main
  process
- extract_rss_cgroup: like _rss, but for each process of the slice
- extract_cgroup: use PVE::QemuServer::CGroup get_memory_stat (this patch)

first, with no KSM active

VMID: 113

pss:        724971520
rss:        733282304
cgroup:     727617536
rss_cgroup: 733282304

Benchmark 1: extract_pss        271.2 ms ±   6.0 ms    [User: 226.3 ms, System: 44.7 ms]
Benchmark 2: extract_rss        267.8 ms ±   3.6 ms    [User: 223.9 ms, System: 43.7 ms]
Benchmark 3: extract_cgroup     273.5 ms ±   6.2 ms    [User: 227.2 ms, System: 46.2 ms]
Benchmark 4: extract_rss_cgroup 270.5 ms ±   3.7 ms    [User: 225.0 ms, System: 45.3 ms]

both reported usage and runtime in the same ballpark

VMID: 838383 (with 48G of memory):

pss:        40561564672
rss:        40566108160
cgroup:     40961339392
rss-cgroup: 40572141568

usage in the same ballpark

Benchmark 1: extract_pss        732.0 ms ±   4.4 ms    [User: 224.8 ms, System: 506.8 ms]

src/PVE/QemuServer.pm
@@ -2325,35 +2325,6 @@ sub vzlist {
     return $vzlist;
 }
 
-# Iterate over all PIDs inside a VMID's cgroup slice and accumulate their PSS (proportional set
-# size) to get a relatively telling effective memory usage of all processes involved with a VM.
-my sub get_vmid_total_cgroup_memory_usage {
-    my ($vmid) = @_;
-
-    my $memory_usage = 0;
-    if (my $procs_fh = IO::File->new("/sys/fs/cgroup/qemu.slice/${vmid}.scope/cgroup.procs", "r")) {
-        while (my $pid = <$procs_fh>) {
-            chomp($pid);
-
-            open(my $smaps_fh, '<', "/proc/${pid}/smaps_rollup")
-                or $!{ENOENT}
-                or die "failed to open PSS memory-stat from process - $!\n";
-            next if !defined($smaps_fh);
-
-            while (my $line = <$smaps_fh>) {
-                if ($line =~ m/^Pss:\s+([0-9]+) kB$/) {
-                    $memory_usage += int($1) * 1024;
-                    last; # end inner while loop, go to next $pid
-                }
-            }
-            close $smaps_fh;
-        }
-        close($procs_fh);
-    }
-
-    return $memory_usage;
-}
-
 our $vmstatus_return_properties = {
     vmid => get_standard_option('pve-vmid'),
     status => {
@@ -2615,9 +2586,11 @@ sub vmstatus {
 
         $d->{uptime} = int(($uptime - $pstat->{starttime}) / $cpuinfo->{user_hz});
 
-        $d->{memhost} = get_vmid_total_cgroup_memory_usage($vmid);
+        my $cgroup = PVE::QemuServer::CGroup->new($vmid);
+        my $cgroup_mem = $cgroup->get_memory_stat();
+        $d->{memhost} = $cgroup_mem->{mem} // 0;
 
-        $d->{mem} = $d->{memhost}; # default to cgroup PSS sum, balloon info can override this below
+        $d->{mem} = $d->{memhost}; # default to cgroup, balloon info can override this below
 
         my $pressures = PVE::ProcFSTools::read_cgroup_pressure("qemu.slice/${vmid}.scope");
         $d->{pressurecpusome} = $pressures->{cpu}->{some}->{avg10} * 1;

commit 61d76518025f0ef3f7c5e6525af0f68e11bfee60

vm status: guard get_memory_stat() call with eval
While in practice, it should be quite rare and unexpected for the
get_memory_stat() to fail, a single failing call should not fail the
whole pvestatd collection for VMs or even the single
'/qemu/{vmid}/status/current' API call for the affected VM, which e.g.
leads to the display for the VM in the UI being inaccessible.

One situation where it can happen is manually invoking the command
given by 'qm showcmd', because the process won't be part of the usual
systemd scope.

Fixes: 551c0078 ("fix #6935: vmstatus: use CGroup for host memory usage")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Link: https://lore.proxmox.com/all/20260122121053.80464-1-f.ebner@proxmox.com

src/PVE/QemuServer.pm
@@ -2589,7 +2589,8 @@ sub vmstatus {
         $d->{uptime} = int(($uptime - $pstat->{starttime}) / $cpuinfo->{user_hz});
 
         my $cgroup = PVE::QemuServer::CGroup->new($vmid);
-        my $cgroup_mem = $cgroup->get_memory_stat();
+        my $cgroup_mem = eval { $cgroup->get_memory_stat() } // {};
+        warn "unable to get memory stat for $vmid - $@" if $@;
         $d->{memhost} = $cgroup_mem->{mem} // 0;
 
         $d->{mem} = $d->{memhost}; # default to cgroup, balloon info can override this below