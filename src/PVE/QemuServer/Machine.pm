proxmox/qemu-server

commit 2e2205a5e18df40188b8add5fc5a771d39b734b1

machine: do not try to set S3/S4 ACPI power state for ARM based virt machine
S3/S4 is rather amd64/x86_64 specific, ARM machines have no concept of
this and fail to start.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/QemuServer/Machine.pm
@@ -466,6 +466,10 @@ sub check_and_pin_machine_string {
 sub get_power_state_flags {
     my ($machine_conf, $version_guard) = @_;
 
+    if ($machine_conf->{type} =~ /^virt/) {
+        return; # virt machines are normally ARM64 ones which have no concept of s3/s4
+    }
+
     my $object =
         $machine_conf->{type} && ($machine_conf->{type} =~ m/q35/) ? "ICH9-LPC" : "PIIX4_PM";
 

commit 9f19f27b31974a6e73ff4ecffd3276fbad1dc35e

machine: avoid undefined value warning to fix tests
Fixes: 2e2205a5 ("machine: do not try to set S3/S4 ACPI power state for ARM based virt machine")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

src/PVE/QemuServer/Machine.pm
@@ -466,7 +466,7 @@ sub check_and_pin_machine_string {
 sub get_power_state_flags {
     my ($machine_conf, $version_guard) = @_;
 
-    if ($machine_conf->{type} =~ /^virt/) {
+    if ($machine_conf->{type} && $machine_conf->{type} =~ /^virt/) {
         return; # virt machines are normally ARM64 ones which have no concept of s3/s4
     }
 