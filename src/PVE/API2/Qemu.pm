proxmox/qemu-server

commit 7546da2cf680035ba33122db770ba20c8f5c3c6f

api: import: follow guest-fsfreeze setting
This branch is triggered when we import-from a disk of a running VM.

Signed-off-by: Maximiliano Sandoval <m.sandoval@proxmox.com>
Link: https://lore.proxmox.com/20260105121702.20884-10-m.sandoval@proxmox.com

src/PVE/API2/Qemu.pm
@@ -374,6 +374,9 @@ my $import_from_volid = sub {
 
         my ($src_storeid) = PVE::Storage::parse_volume_id($src_volid);
 
+        my $qga = $src_conf->{agent}
+            && PVE::QemuServer::Agent::get_qga_key($src_conf, 'guest-fsfreeze') // 1;
+
         return PVE::QemuServer::clone_disk(
             $storecfg,
             $source_info,
@@ -382,7 +385,7 @@ my $import_from_volid = sub {
             $vollist,
             undef,
             undef,
-            $src_conf->{agent},
+            $qga,
             PVE::Storage::get_bandwidth_limit('clone', [$src_storeid, $dest_info->{storage}]),
         );
     };

commit 0a45bf9dc38e1856f98a3cd4770578d02efe1ed7

guest fsfreeze: fix applying default value for configuration sub-property
The && operator has higher precedence than //, so parentheses are
needed.

Fixes: 7546da2c ("api: import: follow guest-fsfreeze setting")
Fixes: 9d359700 ("fix #1964: honor guest-fsfreeze setting for replications and snapshots")
Fixes: 08ae75f5 ("api: clone_vm: follow guest-fsfreeze setting")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

src/PVE/API2/Qemu.pm
@@ -375,7 +375,7 @@ my $import_from_volid = sub {
         my ($src_storeid) = PVE::Storage::parse_volume_id($src_volid);
 
         my $qga = $src_conf->{agent}
-            && PVE::QemuServer::Agent::get_qga_key($src_conf, 'guest-fsfreeze') // 1;
+            && (PVE::QemuServer::Agent::get_qga_key($src_conf, 'guest-fsfreeze') // 1);
 
         return PVE::QemuServer::clone_disk(
             $storecfg,
@@ -4560,7 +4560,7 @@ __PACKAGE__->register_method({
                         if $opt eq 'efidisk0';
 
                     my $qga = $oldconf->{agent}
-                        && PVE::QemuServer::Agent::get_qga_key($oldconf, 'guest-fsfreeze') // 1;
+                        && (PVE::QemuServer::Agent::get_qga_key($oldconf, 'guest-fsfreeze') // 1);
 
                     my $newdrive = PVE::QemuServer::clone_disk(
                         $storecfg,

src/PVE/QemuConfig.pm
@@ -299,7 +299,7 @@ sub __snapshot_check_freeze_needed {
             $running
                 && PVE::QemuServer::Agent::get_qga_key($config, 'enabled')
                 && PVE::QemuServer::Agent::qga_check_running($vmid)
-                && PVE::QemuServer::Agent::get_qga_key($config, 'guest-fsfreeze') // 1,
+                && (PVE::QemuServer::Agent::get_qga_key($config, 'guest-fsfreeze') // 1),
         );
     } else {
         return ($running, 0);