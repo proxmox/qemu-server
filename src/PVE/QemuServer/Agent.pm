proxmox/qemu-server

commit 90fde50a3ed15ab20ac6685c8918268b5e30f43e

agent: add a guest_fsthaw helper
Signed-off-by: Maximiliano Sandoval <m.sandoval@proxmox.com>
Link: https://lore.proxmox.com/20260105121702.20884-5-m.sandoval@proxmox.com

src/PVE/QemuServer/Agent.pm
@@ -268,4 +268,20 @@ sub guest_fsfreeze {
     die "unable to freeze guest fs - unexpected status '$status'\n" if $status ne 'frozen';
 }
 
+=head3 guest_fsthaw
+
+    guest_fsthaw($vmid);
+
+Thaws the file systems of the guest C<$vmid>. Dies if the file systems cannot be thawed.
+
+See C<$guest_fsfreeze> for more details.
+
+=cut
+
+sub guest_fsthaw {
+    my ($vmid) = @_;
+
+    PVE::QemuServer::Monitor::mon_cmd($vmid, "guest-fsfreeze-thaw");
+}
+
 1;

commit adc880910bb8ecb0ed82a9a79100c0e7896b4d3a

agent: fs thaw: check command result
All callers already use eval{} and warn, so this does not change the
execution flow for any callers, but thaw errors will not be quietly
ignored anymore.

Fixes: 90fde50a ("agent: add a guest_fsthaw helper")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Link: https://lore.proxmox.com/20260324135325.120749-2-f.ebner@proxmox.com

src/PVE/QemuServer/Agent.pm
@@ -283,7 +283,10 @@ See C<$guest_fsfreeze> for more details.
 sub guest_fsthaw {
     my ($vmid) = @_;
 
-    PVE::QemuServer::Monitor::mon_cmd($vmid, "guest-fsfreeze-thaw");
+    my $res = PVE::QemuServer::Monitor::mon_cmd($vmid, "guest-fsfreeze-thaw");
+    check_agent_error($res, "unable to thaw guest filesystem");
+
+    return;
 }
 
 1;