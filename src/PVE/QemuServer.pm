proxmox/qemu-server

commit 5b5c576886fbbe79be52c977f9c7ba79a5bfa39f

display warnings for storage errors or if storage no longer exists
Instead of continuing without informing the user, a warning will now be
displayed if the owner of a volume could not be determined due to a
storage error. In addition, an explicit check for the existence of the
underlying storage is added before the ownership check. If the storage
no longer exists, a warning will be displayed, consistent with the
handling of this scenario in other functions.

Signed-off-by: Michael Köppl <m.koeppl@proxmox.com>
Link: https://lore.proxmox.com/20260701141339.181276-11-m.koeppl@proxmox.com
[FE: clarify that the volume is removed from the config]
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

src/PVE/QemuServer.pm
@@ -1557,6 +1557,7 @@ sub vm_is_volid_owner {
     if ($volid !~ m|^/|) {
         my ($path, $owner);
         eval { ($path, $owner) = PVE::Storage::path($storecfg, $volid); };
+        log_warn("ownership of volume '$volid' could not be determined: $@") if $@;
         if ($owner && ($owner == $vmid)) {
             return 1;
         }
@@ -1574,8 +1575,14 @@ sub vmconfig_register_unused_drive {
         delete $conf->{'special-sections'}->{cloudinit};
     } elsif (!drive_is_cdrom($drive)) {
         my $volid = $drive->{file};
-        if (vm_is_volid_owner($storecfg, $vmid, $volid)) {
-            PVE::QemuConfig->add_unused_volume($conf, $volid, $vmid);
+        my ($storeid, undef) = PVE::Storage::parse_volume_id($volid);
+        if (PVE::Storage::storage_config($storecfg, $storeid, 1)) {
+            if (vm_is_volid_owner($storecfg, $vmid, $volid)) {
+                PVE::QemuConfig->add_unused_volume($conf, $volid, $vmid);
+            }
+        } else {
+            log_warn(
+                "storage '$storeid' no longer exists, volume '$volid' will be removed from config");
         }
     }
 }

commit bfd31023dfd2a4a44199a4e886a019c8612d9520

fix #7828: register unused drive: fix failure to handle non-PVE managed volumes
Volumes that are not PVE-managed have no storage associated to them,
and in particular cannot be owned by a VM. When removing such a volume
from the configuration, registering it as unused should be skipped.
This was the case before commit 5b5c5768 ("display warnings for
storage errors or if storage no longer exists"), but the new handling
misses this edge case and causes a failure to parse the volume ID
instead. Fix the regression by using the non-erroring variant when
parsing the volume ID and skipping such volumes again.

Fixes: 5b5c5768 ("display warnings for storage errors or if storage no longer exists")
Analyzed-by: Thomas Ellmenreich <t.ellmenreich@proxmox.com>
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Link: https://lore.proxmox.com/all/20260724081611.11254-1-f.ebner@proxmox.com

src/PVE/QemuServer.pm
@@ -1576,7 +1576,8 @@ sub vmconfig_register_unused_drive {
         delete $conf->{'special-sections'}->{cloudinit};
     } elsif (!drive_is_cdrom($drive)) {
         my $volid = $drive->{file};
-        my ($storeid, undef) = PVE::Storage::parse_volume_id($volid);
+        my ($storeid, undef) = PVE::Storage::parse_volume_id($volid, 1);
+        return if !$storeid; # Not a PVE-managed volume, don't register as unused.
         if (PVE::Storage::storage_config($storecfg, $storeid, 1)) {
             if (vm_is_volid_owner($storecfg, $vmid, $volid)) {
                 PVE::QemuConfig->add_unused_volume($conf, $volid, $vmid);