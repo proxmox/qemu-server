proxmox/qemu-server

commit 698550c2682ed8ab20c3d41a56be79b11063c05a

cpu flags: include Hyper-V enlightenment flags in supported flags
QEMU advertises the Hyper-V enlightenment flags [0] through a different
CPUID range, which query-cpu-model-expansion does not cover by default.

This leads to query_supported_cpu_flags reporting the hv-* flags as
unsupported [1] even though some of them are in the VM-specific flags
and QEMU would accept them when starting a VM.

Enable hv-passthrough in the temporary VM used to query supported flags
for KVM, making it advertise support for the hv-* flags as well.

[0] https://www.qemu.org/docs/master/system/i386/hyperv.html
[1] https://forum.proxmox.com/threads/incorrect-supported-cpu-flags.183722/#post-853902

Signed-off-by: Arthur Bied-Charreton <a.bied-charreton@proxmox.com>
Link: https://lore.proxmox.com/20260522132122.712794-2-a.bied-charreton@proxmox.com

src/PVE/QemuServer.pm
@@ -2982,12 +2982,15 @@ sub query_supported_cpu_flags {
         my $rc = run_command($cmd, noerr => 1, quiet => 0);
         die "QEMU flag querying VM exited with code " . $rc if $rc;
 
+        my $model = { name => $kvm ? 'host' : 'max' };
+        $model->{props} = { 'hv-passthrough' => JSON::true } if $kvm;
+
         eval {
             my $cmd_result = mon_cmd(
                 $fakevmid,
                 'query-cpu-model-expansion',
                 type => 'full',
-                model => { name => $kvm ? 'host' : 'max' },
+                model => $model,
             );
 
             my $props = $cmd_result->{model}->{props};

commit 6c0127e612f6c576888a13f9bfb30874911b804d

fix #7981: cpu flags: query for Hyper-V enlightenments on x86_64 only
'hv-passthrough' is x86-only, so QEMU's ARM target rejects it with
"Parameter 'model.props.hv-passthrough' is unexpected". The whole
expansion then fails, leaving pvestatd unable to populate the
cpuflags-kvm node key on aarch64 hosts. Guard with the arch to prevent
this.

Fixes: 698550c2 ("cpu flags: include Hyper-V enlightenment flags in supported flags")
Signed-off-by: Erik Fastermann <e.fastermann@proxmox.com>
Link: https://lore.proxmox.com/20260831101747.160326-1-e.fastermann@proxmox.com
[FE: slightly improve commit message]
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

src/PVE/QemuServer.pm
@@ -3023,7 +3023,7 @@ sub query_supported_cpu_flags {
         die "QEMU flag querying VM exited with code " . $rc if $rc;
 
         my $model = { name => $kvm ? 'host' : 'max' };
-        $model->{props} = { 'hv-passthrough' => JSON::true } if $kvm;
+        $model->{props} = { 'hv-passthrough' => JSON::true } if $kvm && $arch eq 'x86_64';
 
         eval {
             my $cmd_result = mon_cmd(